c17034ba9ddd32dc2e41e6328306b56b6e565c36
randidiom <randidiom@github.com>
2015-11-22 13:37:36 +0100

    all bugfixes of chunker copied from master to this branch

--- utils/chunker/chunker.c
+++ utils/chunker/chunker.c
@@ -50,24 +50,27 @@ void chunker_load_file_content() {
 }
 
 /**
- * Divides the file into num_chunks chunks and computes the chunk start and end positions.
+ * Divides the file into n_chunks chunks and computes the chunk start and end positions.
  *
- * num_chunks   {int}   Number of chunks or -1 for the machine's core number
- * returns      {char**} Array of char* of size num_chunks+1, indicating the boundaries
+ * n_chunks      {int}   Number of chunks or -1 for the machine's core number
+ * returns      {char**} Array of char* of size n_chunks+1, indicating the boundaries
+ *                       and for -1 changes the value of n_chunks to the actual number.
  *
  * Warning:  Empty chunks (start == end) are possible in rare cases
  */
-char** chunker_compute_chunks(int num_chunks) {
+char** chunker_compute_chunks(int * n_chunks) {
+    int num_chunks = *n_chunks;
 
     int num_threads = omp_get_num_procs();
     if (num_chunks < 1)
+    {
+        *n_chunks  = num_threads;
         num_chunks = num_threads;
-    
+    }
 
     char ** chunks = (char **) calloc(num_chunks+1, sizeof(char*));
     chunks[0] = chunker_buf;
-    chunks[num_chunks] = chunker_buf + chunker_filesize + 1; //last chunk ends at EOF+1
-
+    chunks[num_chunks] = chunker_buf + chunker_filesize; //last chunk ends at EOF+1
 
     long chunk_size = chunker_filesize/num_chunks;
     if (chunker_filesize % num_chunks != 0)
@@ -79,12 +82,12 @@ char** chunker_compute_chunks(int num_chunks) {
 
 
     chunker_buf[chunker_filesize] = '<'; //prevent chunk computation from overshooting
-    
+
     int i;
     if (num_chunks/num_threads > 200)
     {
         #pragma omp parallel for \
-                    schedule(static,num_chunks/num_threads)
+                    schedule(static,num_chunks/num_threads) shared(chunks)
         for (i=1;i < num_chunks; ++i)
         {
             char * locbuf = chunker_buf + chunk_size * i;
@@ -95,21 +98,36 @@ char** chunker_compute_chunks(int num_chunks) {
 
             //printf("locbuf of chunk %i set to %ld in thread %d\n", 
             //        i, (long) (locbuf - chunker_buf), omp_get_thread_num());
+
+            chunks[i] = locbuf;
         }
     } else {
         for (i=1;i < num_chunks; ++i)
         {
             char * locbuf = chunker_buf + chunk_size * i;
 
-            while (*locbuf++ != '<') { /*no other work necessary*/; }
+            while (*locbuf++ != '<')
+                /*no other work necessary*/;
 
             --locbuf; //reset to position of '<'
 
             //printf("locbuf of chunk %i set to %ld.\n", 
             //        i, (long) (locbuf - chunker_buf));
+
+            chunks[i] = locbuf;
         }
     }
+
     return chunks;
+}
+
+void chunker_print_chunk(int i, char* start, char* end) {
+    printf("\n==== start chunk %d ====\n", i);
+    do
+    {
+        putchar(*start);
+    } while(++start != end);
+    printf("\n==== end chunk %d ====\n", i);
 }
 
 
