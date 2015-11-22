a7c0664b8a6a2c0fdda742a9ebf8408553d58205
Tobias Kaiser <tobias@localhost.localdomain>
2015-11-22 16:28:02 +0100

    mediator test file added

--- src/multi_dfa/test/mediator_test.c
+++ src/multi_dfa/test/mediator_test.c
@@ -1,25 +1,23 @@
 #include"../mediator.h"
-#include"../../parser/tokenstream.h"
 
-int main(void)
+void main(void)
 {
 
 	Tokenstream * ts = parse_file("/home/tobias/i_ti_eytsch/m1/dphpc/git/xml/test10M",
-			"/home/tobias/i_ti_eytsch/m1/dphpc/git/xml/tokens", 1);
+			"/home/tobias/i_ti_eytsch/m1/dphpc/git/xml/tokens", 6);
 	Token * t = NULL;
 	for(;;)
 	{
-		if((t = get_token(ts, t)) != NULL)
+		if((t = ts_get_token(ts, t)) != NULL)
 		{
 			printf("tokentype: %d, token: %.*s\n", t->type, t->end - t->begin, t->begin);
 		}
 		else
 		{
-			if(ts->next == NULL)
+			if(ts->next = NULL)
 				break;
 			ts = ts->next;
 			t = NULL;
 		}
 	}
-	return 0;
 }
