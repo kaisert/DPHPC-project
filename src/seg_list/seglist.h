104c390c51fe4833c3a2aea4d08680dba5b4f38f
Stefan Dietiker <stefan.dietiker@gmail.com>
2015-11-22 18:19:18 +0100

    header for segmented list

---
+++ src/seg_list/seglist.h
@@ -0,0 +1,46 @@
+#ifndef H_SEGLIST
+#define H_SEGLIST
+
+#include<stddef.h>
+
+// TYPES
+// TODO: define struct
+typedef struct {
+} seglist_t;
+
+// TODO: exact types TBD
+typedef void* seglist_seg_t;
+typedef void* seglist_read_iter_t;
+
+
+//
+void seglist_create(
+        seglist_t* seglist,
+        size_t buf_size,
+        size_t elem_size);
+
+seglist_seg_t seglist_create_segment( // create new segment
+        seglist);
+
+void seglist_append(        // only operates on segment seg
+        seglist_seg_t* seg,
+        void* elem);
+
+size_t seglist_append_n(    // only operates on segment seg
+        seglist_seg_t* seg,
+        void* elem,
+        size_t n);
+
+void seglist_create_read_iter(  // creates a read iterator for the entire list
+        seglist_read_iter_t* read_iter,
+        seglist_t seglist);
+
+void* seglist_get_next(
+        seglist_read_iter_t* read_iter);
+
+size_t seglist_get_next_n(
+        seglist_read_iter_t* read_iter,
+        void **start,
+        size_t n);
+
+#endif
