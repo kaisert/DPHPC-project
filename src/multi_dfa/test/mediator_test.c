e979df343c64a5cdfe0051394db7af601af02bbf
Tobias Kaiser <tobias@localhost.localdomain>
2015-11-23 14:21:51 +0100

    added testfiles

--- src/multi_dfa/test/mediator_test.c
+++ src/multi_dfa/test/mediator_test.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 
-	Tokenstream * ts = parse_file("/home/tobias/i_ti_eytsch/m1/dphpc/git/xml/test10M",
-			"/home/tobias/i_ti_eytsch/m1/dphpc/git/xml/tokens", 6);
+	Tokenstream * ts = parse_file("./test10M",
+			"./tokens", 1);
 	Token * t = NULL;
 	for(;;)
 	{
