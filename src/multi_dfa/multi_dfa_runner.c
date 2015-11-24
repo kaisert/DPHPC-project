884935fd415003cc56da1e9b393c408a54994547
Stefan Dietiker <stefan.dietiker@gmail.com>
2015-11-24 22:12:05 +0100

    end of day commit: MDFA runner is still BUGGY

--- src/multi_dfa/multi_dfa_runner.c
+++ src/multi_dfa/multi_dfa_runner.c
@@ -1,6 +1,7 @@
 #include<omp.h>
 #include<stdio.h>
 #include<fcntl.h>
+#include<unistd.h>
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<sys/mman.h>
@@ -28,7 +29,7 @@ int main(int argc, char* argv[]) {
 #define ARG_OUTPUT argv[4]
 
     multi_dfa_t multi_dfa;
-    int fd_xml, n_threads, no_chunks;
+    int fd_xml, n_threads, no_chunks = 0;
     struct stat f_xml_stat;
     off_t xml_len;
 
@@ -38,19 +39,19 @@ int main(int argc, char* argv[]) {
     multi_dfa_init(f_multi_dfa, &multi_dfa);
     fclose(f_multi_dfa);
 
-    fd_xml = open(ARG_XML, O_RDONLY);
+    fd_xml = open(ARG_XML, O_RDWR);
     if(fd_xml < 0) panic("could not open xml file.");
     fstat(fd_xml, &f_xml_stat);
-    xml_len = (size_t) f_xml_stat.st_size;
+    xml_len = (off_t) f_xml_stat.st_size;
 
     char* xml_buf =
-    mmap(   0,                   // address
-            xml_len,             // length
-            PROT_READ,           // prot flags
-            0,                   // 
-            fd_xml,              // file decriptor
-            0);                  // offset in file
-    if(xml_buf == MAP_FAILED) panic("could not map file");
+    mmap(   NULL,                  // address
+            xml_len,               // length
+            PROT_READ,             // prot flags
+            MAP_FILE | MAP_SHARED, // flags
+            fd_xml,                // file decriptor
+            0);                    // offset in file
+    if(xml_buf == MAP_FAILED) panic("mmap failed: ");
     
 
     n_threads = multi_dfa.no_dfa;
@@ -59,18 +60,22 @@ int main(int argc, char* argv[]) {
     char* chunks[n_threads+1];
     no_chunks = bufsplit_split_xml_stream(xml_buf, xml_len, n_threads, chunks);
 
+    printf("%d\n", no_chunks);
     // we want at least as many (meaningful) chunks as threads
     if(no_chunks != n_threads) panic("your xml is too small!");
    
     // initialize token stream
-	Tokenstream token_stream[n_threads];
+	Tokenstream* token_stream[n_threads];
+    token_stream[0] = tokenstream_init((4096) / sizeof(Token));
+
 	for(int i = 0; i < n_threads; ++i)
 	{
-		create_tokenstream(&token_stream[i], (4096) / sizeof(Token));
+        if( i > 0 )
+            token_stream[i] = tokenstream_append(token_stream[i-1]);
 
 		if(i != n_threads -1)
 		{
-			token_stream[i].next = &token_stream[i+1];
+			token_stream[i]->next = token_stream[i+1];
 		}
 	}
 
@@ -88,7 +93,7 @@ int main(int argc, char* argv[]) {
 
 		parser = alloc_parser(chunk_begin, chunk_end);
 		init_parser(parser, map);
-		Tokenstream * ts_iter = token_stream + tid; 
+		Tokenstream * ts_iter = token_stream[tid]; 
 		Token * current = get_new_token_pointer(&ts_iter);
 		while(get_next_token(parser, current) == 1)
 		{
@@ -104,15 +109,15 @@ int main(int argc, char* argv[]) {
 
 #pragma omp parallel num_threads(n_threads) firstprivate(token_stream)
     {
-        Tokenstream* ts_iter = token_stream;
+        Tokenstream* ts_iter = token_stream[0];
         int tid = omp_get_thread_num() % n_threads;
         state_t q_cur = multi_dfa.start_states[tid];
         dfa_t dfa = multi_dfa.dfa_list[tid];
 
         // allocate stack
         int stack_pos = 0;
-        state_t* dfa_stack = malloc(STACK_SIZE * sizeof(state_t));
-        dfa_stack[stack_pos++] = q_cur;
+        state_t dfa_stack[STACK_SIZE];
+        dfa_stack[stack_pos] = q_cur;
 
         // iterate over tokenstream
         while(ts_iter != NULL) {
@@ -120,11 +125,20 @@ int main(int argc, char* argv[]) {
 
             while(t != ts_iter->end) {
                 if(t->type < 0) {
-                    if(stack_pos > 0)
+                    if(stack_pos > 0) {
                         stack_pos--;
+                        if(((int) dfa_stack[stack_pos]) < 0) {
+                            printf("closing match: %.16s (pos: %ld)\n",
+                                    t->begin, t->begin - xml_buf);
+                        }
+                    }
                 } else if(t->type > 0) {
                     dfa_stack[stack_pos+1] = multi_dfa_delta(
                             dfa, dfa_stack[stack_pos], t->type);
+                    if(dfa_stack[stack_pos] < 0) {
+                            printf("open match: %.16s (pos: %ld)\n",
+                                    t->begin, t->begin - xml_buf);
+                    }
                     stack_pos++;
                 }
                 t++;
@@ -135,12 +149,13 @@ int main(int argc, char* argv[]) {
     // stop measurements
     
     // output result
+    /*
     FILE* f_output = fopen(argv[4], "w");
     if(!f_output) {
         perror("could not open output file.");
     }
     
-    Tokenstream* ts_iter = token_stream;
+    Tokenstream* ts_iter = token_stream[0];
     while(ts_iter != NULL) {
         Token* t = ts_iter->begin;
         while(t != ts_iter->end) {
@@ -150,9 +165,12 @@ int main(int argc, char* argv[]) {
 
         ts_iter = ts_iter->next;
     }
+    */
 
-    fclose(f_output);
+    close(fd_xml);
+    // fclose(f_output);
     // destroy tokenstream
-    destroy_tokenstream(token_stream);
+    multi_dfa_free(&multi_dfa);
+    destroy_tokenstreams(token_stream[0]);
     // tobias will write a function
 }
