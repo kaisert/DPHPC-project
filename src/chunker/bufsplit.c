90237786cef3ba3e35cdec947f33c4ce5d70d8fe
Stefan Dietiker <stefan.dietiker@gmail.com>
2015-11-24 22:10:03 +0100

    corrected offby1error in bufsplit

--- src/chunker/bufsplit.c
+++ src/chunker/bufsplit.c
@@ -6,10 +6,11 @@ int bufsplit_split_xml_stream(
         size_t no_chunks,
         char** offsets) {
 
-    int chunk_count = 0;
+    int chunk_count = 1;
     char* cur = stream, *cur_chunk = stream, *end = stream + len;
     size_t min_chunk_len = len/no_chunks;
-
+    
+    offsets[0] = stream;
     while(cur != end) {
         if(*cur == '<') {
             if(((size_t) (cur - cur_chunk)) >= min_chunk_len) {
