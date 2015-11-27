b6ac715b93f4cb1b525220b90e7d28e16b0a3390
Stefan Dietiker <stefan.dietiker@gmail.com>
2015-11-27 17:40:31 +0100

    Deflator concept

---
+++ src/compression/Deflator.cpp
@@ -0,0 +1,10 @@
+
+
+template<typename T_elem>
+class Deflator {
+    template<typename InputIterator, typename BackInserter>
+    InputIterator deflate(RandIter begin, RandIter end, BackInserter target) {
+    }
+
+    int end();
+}
