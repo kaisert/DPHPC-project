57eed0fcdbffcb77e70e34d611077157a78c9df9
Stefan Dietiker <stefan.dietiker@gmail.com>
2015-11-27 14:31:30 +0100

    new interface design for compression

--- src/compression/compression.h
+++ src/compression/compression.h
@@ -10,19 +10,15 @@
 #ifndef H_COMPRESSION
 #define H_COMPRESSION
 
-#include<steddef.h>
+#include<stddef.h>
+#include<zlib.h>
 
-#ifdef COMPR_Z_LIB
-    #include<zlib.h>
-#endif
+// we use the same API as Z_LIB
+#define COMPR_FLUSH Z_FLUSH
+#define COMPR_NO_FLUSH Z_NO_FLUSH
 
-
-#ifdef COMPR_Z_LIB
-    #define COMPR_FLUSH Z_FLUSH
-    #define COMPR_NO_FLUSH Z_NO_FLUSH
-#elif COMPR_BITMASK
-    // TODO
-#endif
+// For small return values, we use the same values as Z_LIB
+#define COMPR_INIT_ERROR -129
 
 // TYPES
 typedef struct compression_stream_struct {
@@ -32,11 +28,7 @@ typedef struct compression_stream_struct {
     size_t out_available;
     char* out;
     // Specific to implementation
-#ifdef COMPR_Z_LIB
-    z_stream zstream;
-#elif COMPR_BITMASK
-    // TODO
-#endif
+    void* stream_state;
 } compression_stream_t;
 
 
@@ -45,31 +37,41 @@ typedef int (*compression_func_t) (compression_stream_t*, bool);
 typedef void (*compression_free_func_t) (void*, bool) ;
 
 
-extern void* compression_inflate_init( /// returns handle for compression
+/*
+ * All functions return code for state. Positive values indicate succes and
+ * possibly and special state. Negative values indicate an error state.
+ *
+ * For _init()-funtions, the user is responsible for the memory management of
+ * the structure that is passed to the function.
+ */
+
+extern int compression_inflate_init( /// initializes handle for inflation
         compression_stream_t* compression_stream
         );
 
 
-extern void* compression_deflate_init( /// returns handle for compression
+extern int compression_deflate_init( /// initializes handle for deflation
         compression_stream_t* compression_stream
         );
 
 
-extern int compression_deflate( /// returns the actual number of bytes read
+extern int compression_deflate( /// returns error state
+        compression_stream_t* compr_stream,
+        int flags /// e.g., COMPR_FLUSH
+        );
+
+
+extern int compression_inflate(  /// returns error state
         compression_stream_t* compr_stream,
         int flags
         );
 
 
-extern int compression_inflate(  /// returns the actual number of bytes written
-        compression_stream_t* compr_stream,
-        int flags
+extern void compression_inflate_end( /// 
+        compression_stream_t* compr_stream
         );
 
-
-extern void compression_free(
-        void* handle,
-        bool inflate
+extern void compression_deflate_end( /// 
+        compression_stream_t* compr_stream
         );
-
 #endif
