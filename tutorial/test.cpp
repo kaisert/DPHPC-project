a640ac65d7e82e783529153e72804c0328346cc4
randidiom <randidiom@github.com>
2015-12-04 15:25:32 +0100

    removed zlib, tutorial scripts extended

--- tutorial/test.cpp
+++ tutorial/test.cpp
@@ -5,6 +5,9 @@ int main(int argc, char** argv)
 {
 	int nthreads,tid;
 
+    for (int i=0;i<argc;++i)
+        printf("%s\n", argv[i]);
+
 	#pragma omp parallel private(nthreads, tid)
 	{
 		tid = omp_get_thread_num();
