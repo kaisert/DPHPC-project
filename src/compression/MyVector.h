2a0e7c0a777525f7611f98c1281375179ef98c37
Tobias Kaiser <tobias@localhost.localdomain>
2015-12-08 14:50:52 +0100

    added myvector

---
+++ src/compression/MyVector.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_WRAPPER
+#define VECTOR_WRAPPER
+
+#include<vector>
+
+template<typename T>
+class MyVector : public std::vector<T> {
+private:
+    typedef typename std::vector<T>::const_iterator c_iter;
+public:
+    inline void init(int i){}
+    inline void flush(){}
+};
+
+#endif
