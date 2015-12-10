13f9251159da081eff53dc571bb08e0029b364d6
randidiom <randidiom@github.com>
2015-12-10 18:18:11 +0100

    compilation scripts with added vecreport flags for icc

--- src/benchmark/icc.sh
+++ src/benchmark/icc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-icc -fopenmp -std=c++11 -mmic -Wall -O3 directoryTiming.cpp -o test.mic
+icc -fopenmp -std=c++11 -mmic -Wall -qopt-report3 -qopt-report-phase=vec -O3 directoryTiming.cpp -o test.mic
--- src/xeon_phi_compile.sh
+++ src/xeon_phi_compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-icc -fopenmp -std=c++11 -O3 -mmic main.cpp parser/token_list.cpp parser/lexer.cpp parser/map.cpp parser/parser.cpp multi_dfa/MultiDFA.cpp chunker/bufsplit.cpp timing/GlobalTicToc.cpp -o test.mic
+icc -fopenmp -std=c++11 -mmic -Wall -qopt-report3 -qopt-report-phase=vec -O3 main.cpp parser/token_list.cpp parser/lexer.cpp parser/map.cpp parser/parser.cpp multi_dfa/MultiDFA.cpp chunker/bufsplit.cpp -o test.mic
