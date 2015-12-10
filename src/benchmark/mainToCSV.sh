4c76f040ef6d7a1c1403e9b9027edf73d0917295
randidiom <randidiom@github.com>
2015-12-10 15:58:45 +0100

    CSV converter for the main application's output and experiment results 9.12.15

---
+++ src/benchmark/mainToCSV.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z $1 ]; then
+    echo 'This script converts the raw outputs of our main application to a CSV file for easier evaluation'
+    echo 'usage: ./mainToCSV.sh raw_input.txt'
+    exit
+fi
+
+
+echo "XML,MDFA,TokenContainer,threads,tokens,loading,chunking,tokenizer,matcher,writeback,total time" > "$1.csv"
+cat $1 | tr '\n' '!' | sed 's/!![!]*/\n/g' | tr '!' ',' | sed 's/,,/\n/g' | sed 's/[^:]*: \([^,]*\)[^:]*: \([^,]*\),[^=]*=\([^,]*\),[^:]*: \([0-9]*\)[^:]*: \([0-9]*\)/\1,\2,\3,\4,\5:::/' | sed 's/:::\([0-9]*\)[^:]*:[^:]*: \([0-9]*\)[^:]*: \([0-9]*\)[^:]*: \([0-9]*\)[^:]*: \([0-9]*\)[^:]*: \([0-9]*\)[^:]*: \([0-9]*\).*/\1,\2,\3,\4,\5,\6,\7/' >> "$1.csv"
